234b38873bca598c3f24438a8965d26c1e2a3180
matelakjakub <73526507+matelakjakub@users.noreply.github.com>
2021-02-22 11:09:42 +0100

    Dodanie zmiennej a
    Działania i wywoływanie

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-a=2
+a= 10
+b= 5
+c= 2
 
-print(a)
+dzielenie=b/c
+print(dzielenie)
+print(type(dzielenie))
+
+dodawanie = a+b
+dzielenie_calkowite = b // c
+potegowanie = a ** c
+
+print(dodawanie)
+print(dzielenie_calkowite)
+print(potegowanie)
+
