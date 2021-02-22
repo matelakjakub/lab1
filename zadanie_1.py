e2e06290e66b749c2ea1678f702d37e7491960d1
matelakjakub <73526507+matelakjakub@users.noreply.github.com>
2021-02-22 11:59:02 +0100

    zadanie 1 zadanie 2 zadanie 3 zadanie 4

--- zadanie_1.py
+++ zadanie_1.py
@@ -1,6 +1,47 @@
+from math import *
+
 #zad1
 a = 2
 b = 'test'
 
 print(a)
 print(b)
+
+#zad2
+x = 4
+y = 6
+z = 8
+h = 12
+i = 16
+j = 24
+dodawanie = x+y+z
+odejmowanie = z-y
+mnożenie = x*y
+dzielenie = z/x
+dzieleniecalkowite= z//y
+potegowanie=pow(y,x)
+pierwiastkowanie=sqrt(x)
+
+print(dodawanie, odejmowanie, mnożenie, dzielenie, dzieleniecalkowite, potegowanie, pierwiastkowanie)
+
+#zad3
+
+x+=4
+y*=2
+z-=3
+h/=2
+i**=3
+j%=5
+
+print(x)
+print(y)
+print(z)
+print(h)
+print(i)
+print(j)
+
+#zad 4
+obl1 = pow(e,10)
+print(obl1)
+obl2 = pow(ln(5+pow(sin,2)*8),1/6)
+print(obl2)
